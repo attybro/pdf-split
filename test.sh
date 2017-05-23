19bb680447b4332f067bffd5ad4620fa1e1d1a84
attybro <attybro@libero.it>
2017-05-23 11:03:37 +0200

    2017-05-23[AB] - Added Linux script and config file

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Split script
 
+u_folder="split_pdf"
+u_zip_file="split_pdf.zip"
+
 #variabili
 u_start=1
 u_end=2;
@@ -13,6 +16,21 @@ echo "New Splitted PDF Name List: $1"
 echo "Complete PDF Name: $2"
 echo "Total Number of Parameters : $#"
 echo "----------------------------------------"
+
+if [ -f "$u_folder" ]
+  then 
+  rm -rf $u_folder
+fi
+mkdir split_pdf
+
+if [ -f "$u_zip_file" ]
+  then 
+  rm $u_zip_file
+fi
+
+
+cp $1 split_pdf
+cp $2 split_pdf
 
 #controlli
 echo "INFO: Procedure Started"
@@ -43,7 +61,7 @@ then
 	echo "ERROR: file $2 is NOT present"
 	exit
     fi    
-    
+   
 else
     echo "ERROR: Please USE-> $0 namefile.txt namefile.pdf"
     exit
@@ -52,18 +70,19 @@ fi
 #split
 echo "SPLIT Started.."
 echo " Generating:"
-while IFS='' read -r line || [[ -n "$line" ]]; do   
-  if $(pdftk $2 cat $u_start-$u_end output $line)
-      then
-	echo "  $u_counter) $line [OK]"
-      else
-	echo "  $u_counter) $line [NOK]"
-    fi
+while IFS='' read -r line || [[ -n "$line" ]]; do  
+#  if $(pdftk $2 cat $u_start-$u_end output "split_pdf/$line")
+#      then
+#	echo "  $u_counter) $line [OK]"
+#      else
+#	echo "  $u_counter) $line [NOK]"
+#    fi
     u_counter=$((u_counter+1))
     u_start=$((u_start+2))
     u_end=$((u_end+2))
 done < "$1"
 echo " Generation ended"
 echo "SPLIT ended."
-
+zip -r split_pdf.zip split_pdf
+rm -rf split_pdf
 echo "INFO: Procedure Ended with $((u_counter-1)) new PDF(s)"
